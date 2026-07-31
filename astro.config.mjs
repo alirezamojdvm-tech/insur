627bb2276148b989c572595c5c7c4ee2e1999917
Alireza Mohammadi Jafari <alireza.mo.j.dvm@gmail.com>
2026-07-31 15:56:38 +0330

    jome

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap'; // اگه Sitemap می‌خوای
 
 export default defineConfig({
-  // آدرس نهایی سایت (برای ساخت لینک‌های مطلق و Sitemap ضروریه)
   site: 'https://insur.hsc-co.ir',
-  
-  // افزونه‌ها (مثل Sitemap)
-  integrations: [sitemap()],
-  
-  // (اختیاری) اگه می‌خوای خروجی کاملاً استاتیک باشه (پیش‌فرض Astro همینطوره)
   output: 'static',
+  trailingSlash: 'ignore', // تغییر از 'always' به 'ignore'
+  build: {
+    format: 'directory',
+  },
 });
